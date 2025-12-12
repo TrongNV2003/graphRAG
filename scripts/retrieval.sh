7aa57f0cd8d5875eba4eff0943097b22e1c5ccb5
Trong Nguyen <trongnv.bi12-449@st.usth.edu.vn>
2025-12-12 17:37:22 +0700

    remove files

--- scripts/retrieval.sh
+++
@@ -1,7 +0,0 @@
-export PYTHONPATH=$PYTHONPATH:$PWD
-
-
-python -m graphRAG.retrieval \
-    --query "Who was the Queen of England?" \
-
-# export PYTHONPATH=$PYTHONPATH:/home/trongnv130/Desktop/graphRAG
--- scripts/visualization.sh
+++
@@ -1,6 +0,0 @@
-export PYTHONPATH=$PYTHONPATH:$PWD
-
-
-python -m graphRAG.visualization \
-    --limit 200 \
-    --visualize_type "2d" \
